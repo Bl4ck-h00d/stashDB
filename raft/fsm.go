fd948b34cf29b4c70b7a18334ccbb1ff7c1e161f
Rohan <Rohan>
2025-03-13 11:22:08 +0530

    test

--- raft/fsm.go
+++ raft/fsm.go
@@ -100,105 +100,7 @@ func (f *RaftFSM) Restore(rc io.ReadCloser) error {
 	return nil
 }
 
-// func (f *RaftFSM) Apply(l *raft.Log) interface{} {
-// 	var event protobuf.Event
-// 	err := proto.Unmarshal(l.Data, &event)
-// 	if err != nil {
-// 		// If binary unmarshaling fails, try JSON unmarshaling
-// 		fmt.Printf("Binary Unmarshal failed, trying JSON: %v\n", err)
-// 		err = protojson.Unmarshal(l.Data, &event)
-// 		if err != nil {
-// 			fmt.Printf("JSON Unmarshal also failed: %v\n", err)
-// 			return nil
-// 		}
-// 	}
 
-// 	switch event.Type {
-// 	case protobuf.EventType_Join:
-// 		data, err := marshaler.MarshalAny(event.Message)
-
-// 		if err != nil {
-// 			slog.Error("failed to marshal join request", slog.Any("error", err))
-// 			return err
-// 		}
-// 		if data == nil {
-// 			err = errors.New("nil")
-// 			slog.Error("request is nil", slog.String("type", event.Type.String()))
-// 			return err
-// 		}
-
-// 		req := data.(*protobuf.SetMetadataRequest)
-
-// 		res := f.applySetMetadata(req.Id, req.Metadata)
-
-// 		if res == nil {
-// 			f.eventCh <- &event
-// 		}
-
-// 		return res
-// 	case protobuf.EventType_Leave:
-// 		data, err := marshaler.MarshalAny(event.Message)
-// 		if err != nil {
-// 			slog.Error("failed to unmarshal leave request", slog.Any("error", err))
-// 			return err
-// 		}
-// 		req := *data.(*protobuf.DeleteMetadataRequest)
-// 		res := f.applyDeleteMetadata(req.Id)
-
-// 		if res == nil {
-// 			f.eventCh <- &event
-// 		}
-
-// 		return res
-
-// 	case protobuf.EventType_Create:
-// 		data, err := marshaler.MarshalAny(event.Message)
-// 		if err != nil {
-// 			slog.Error("failed to unmarshal create bucket request", slog.Any("error", err))
-// 			return err
-// 		}
-
-// 		req := *data.(*protobuf.CreateBucketRequest)
-// 		res := f.applyCreateBucket(req.Name)
-// 		if res == nil {
-// 			f.eventCh <- &event
-// 		}
-
-// 		return res
-// 	case protobuf.EventType_Set:
-// 		data, err := marshaler.MarshalAny(event.Message)
-// 		if err != nil {
-// 			slog.Error("failed to unmarshal set request", slog.Any("error", err))
-// 			return err
-// 		}
-// 		req := *data.(*protobuf.SetRequest)
-// 		res := f.applySet(req.Bucket, req.Key, []byte(req.Value))
-// 		if res == nil {
-// 			f.eventCh <- &event
-// 		}
-
-// 		return res
-// 	case protobuf.EventType_Delete:
-// 		data, err := marshaler.MarshalAny(event.Message)
-// 		if err != nil {
-// 			slog.Error("failed to unmarshal delete request", slog.Any("error", err))
-// 			return err
-// 		}
-
-// 		req := *data.(*protobuf.DeleteRequest)
-// 		res := f.applyDelete(req.Bucket, req.Key)
-// 		if res == nil {
-// 			f.eventCh <- &event
-// 		}
-
-// 		return res
-// 	default:
-// 		err = errors.New("command type not support")
-// 		slog.Error("unsupported command", slog.String("type", event.Type.String()))
-// 		return err
-// 	}
-// 	return nil
-// }
 
 func (f *RaftFSM) applyCreateBucket(name string) error {
 	err := f.store.CreateBucket(name)
@@ -223,14 +125,6 @@ func (f *RaftFSM) Get(bucket, key string) (*protobuf.GetResponse, error) {
 	return protoRes, nil
 }
 
-// func (f *RaftFSM) applySet(bucket, key string, value []byte) error {
-// 	slog.Debug("SET Request", slog.String("bucket", bucket), slog.String("key", key), slog.String("value", string(value)))
-// 	err := f.store.Set(bucket, key, value)
-// 	if err != nil {
-// 		slog.Error("failed to set key", slog.String("key", key), slog.String("bucket", bucket), slog.Any("error", err))
-// 	}
-// 	return nil
-// }
 
 func (f *RaftFSM) applyDelete(bucket, key string) error {
 	err := f.store.Delete(bucket, key)
@@ -325,54 +219,7 @@ func (s *RaftFSM) Snapshot() (raft.FSMSnapshot, error) {
 	return &FSMSnapshot{store: s.store}, nil
 }
 
-// func (s *RaftFSM) Restore(rc io.ReadCloser) error {
-// 	start := time.Now()
-// 	slog.Info("start to restore items")
 
-// 	defer func() {
-// 		err := rc.Close()
-// 		if err != nil {
-// 			slog.Error("failed to close reader", slog.Any("error", err))
-// 		}
-// 	}()
-
-// 	data, err := ioutil.ReadAll(rc)
-// 	if err != nil {
-// 		slog.Error("failed to read from snapshot", slog.Any("error", err))
-// 		return err
-// 	}
-
-// 	keyCount := uint64(0)
-// 	buff := proto.NewBuffer(data)
-// 	for {
-// 		kvp := &protobuf.KeyValuePair{}
-// 		err = buff.DecodeMessage(kvp)
-// 		if err == io.ErrUnexpectedEOF || err == io.EOF {
-// 			break // Finished reading all items
-// 		}
-// 		if err != nil {
-// 			slog.Error("failed to decode key-value pair", slog.Any("error", err))
-// 			return err
-// 		}
-
-// 		// Apply item to the store, including the bucket and key
-// 		if kvp.Bucket == "" {
-// 			slog.Error("bucket name is empty", slog.String("key", kvp.Key))
-// 			return errors.New("bucket name cannot be empty")
-// 		}
-// 		err = s.store.Set(kvp.Bucket, kvp.Key, kvp.Value)
-// 		if err != nil {
-// 			slog.Error("failed to set key-value pair in the store", slog.String("bucket", kvp.Bucket), slog.String("key", kvp.Key), slog.Any("error", err))
-// 			return err
-// 		}
-
-// 		slog.Debug("restored item", slog.String("bucket", kvp.Bucket), slog.String("key", kvp.Key))
-// 		keyCount++
-// 	}
-
-// 	slog.Info("finished restoring items", slog.Uint64("count", keyCount), slog.Float64("time", float64(time.Since(start))/float64(time.Second)))
-// 	return nil
-// }
 
 func (s *FSMSnapshot) Persist(sink raft.SnapshotSink) error {
 	start := time.Now()
